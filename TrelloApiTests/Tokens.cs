4b2e72d93123eff019a72177c2a90b2790fdf9ce
Jan Křížek <jkrizek911@gmail.com>
2025-04-12 19:06:41 +0200

    Remove secrets

--- TrelloApiTests/Tokens.cs
+++ TrelloApiTests/Tokens.cs
@@ -3,12 +3,7 @@
     public class Tokens
     {
         //Authentication        
-        public static string trelloApiKey = "84bb112aa652706cfe7fbe0d99b657c3";
-        public static string trelloApiToken = "ATTA6c0cd2f659df34cea5b6af359816b15e7b0aa9769389cb1cd4ae84eeab921075E1F01590";
-        public string memberId = "631317316ef1a7013a22c285";
-        public string calendarKey = "65a6s4df676asdf";
-
-        public string trelloKeyToken1 = $"&key={trelloApiKey}&token={trelloApiToken}";
-        public string trelloKeyToken2 = $"?key={trelloApiKey}&token={trelloApiToken}";
+        public string trelloKeyToken1 = "";
+        public string trelloKeyToken2 = "";
     }
 }
